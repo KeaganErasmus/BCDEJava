3bd0d0197deaf681c0bbd4a4e242413d239a8e33
keagan erasmus <keaganerasmus12@gmail.com>
2023-03-14 22:54:06 +1300

    Almost got all the files implemented.

--- EyeballMaze/src/Game.java
+++ EyeballMaze/src/Game.java
@@ -1,26 +1,38 @@
 public class Game implements ILevelHolder{
+    private int levelWidth;
+    private int levelHeight;
+    private int levelCount;
+    public int[][] newLevel;
+
     @Override
     public void addLevel(int height, int width) {
+        levelWidth = width;
+        levelHeight = height;
+        levelCount += 1;
 
+        for(int row = 0; row < levelHeight; row++){
+            for (int col = 0; col < levelWidth; col++){
+                newLevel[row][col] = row + col * width;
+            }
+        }
     }
 
     @Override
     public int getLevelWidth() {
-        return 0;
+        return levelWidth;
     }
 
     @Override
     public int getLevelHeight() {
-        return 0;
+        return levelHeight;
     }
 
     @Override
     public void setLevel(int levelNumber) {
-
     }
 
     @Override
     public int getLevelCount() {
-        return 0;
+        return levelCount;
     }
 }
---
+++ EyeballMaze/src/SquareHolder.java
@@ -0,0 +1,16 @@
+public class SquareHolder implements ISquareHolder {
+    @Override
+    public void addSquare(Square square, int row, int column) {
+
+    }
+
+    @Override
+    public Color getColorAt(int row, int column) {
+        return null;
+    }
+
+    @Override
+    public Shape getShapeAt(int row, int column) {
+        return null;
+    }
+}
