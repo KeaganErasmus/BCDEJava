6bdb73743a1c62d57f5ead0a76c079579ecaf82a
keagan erasmus <keaganerasmus12@gmail.com>
2023-03-14 13:55:13 +1300

    fixed silly spelling issue.

--- Assessment1/src/Main.java
+++ Assessment1/src/Main.java
@@ -1,18 +1,21 @@
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args) {
 
+        Array numVehicles[];
         // Classes
         Vehicle golf = new Vehicle();
         golf.getBrand("Volkswagen");
         golf.getModel("Golf");
         golf.getNumWheels(4);
         golf.turnOn(true);
-
+        
         System.out.println();
 
         Vehicle tricycle = new Vehicle();
-        golf.getBrand("Mobo Cruiser");
-        golf.getModel("Pro Adult Tricycle");
-        golf.getNumWheels(3);
+        tricycle.getBrand("Mobo Cruiser");
+        tricycle.getModel("Pro Adult Tricycle");
+        tricycle.getNumWheels(3);
     }
 }
