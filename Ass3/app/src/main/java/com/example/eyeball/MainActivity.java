17d3be2b5a9f08f183d5734d9454c159ca57b310
KeaganErasmus <keaganerasmus12@gmail.com>
2023-05-18 19:55:21 +1200

    ass3 started

--- Ass3/app/src/main/java/com/example/eyeball/MainActivity.java
+++ Ass3/app/src/main/java/com/example/eyeball/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.eyeball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView goal = findViewById(R.id.Goal);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void move(View view){
+        Toast.makeText(this, "Goal", Toast.LENGTH_SHORT).show();
     }
 }
