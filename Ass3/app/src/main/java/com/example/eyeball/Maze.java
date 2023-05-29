d8d694d2d86e731a80c3250085add5a692a7cc56
KeaganErasmus <keaganerasmus12@gmail.com>
2023-05-29 17:42:54 +1200

    Created a fork to try something new

--- Ass3/app/src/main/java/com/example/eyeball/Maze.java
+++ Ass3/app/src/main/java/com/example/eyeball/Maze.java
@@ -1,6 +1,28 @@
 package com.example.eyeball;
 
+import android.widget.ImageView;
+
 public class Maze {
+//    final ImageView[] imageViews = new ImageView[5];
+//    final int[] eyeBallImages = new int[4];
+//    final int[] imageSrcs = new int[5];
+//    imageViews[0] = findViewById(R.id.imageView1);
+//    imageViews[1] = findViewById(R.id.imageView2);
+//    imageViews[2] = findViewById(R.id.imageView3);
+//    imageViews[3] = findViewById(R.id.imageView4);
+//    imageViews[4] = findViewById(R.id.imageView5);
+//
+//    imageSrcs[0] = R.drawable.b_cross;
+//    imageSrcs[1] = R.drawable.r_flower;
+//    imageSrcs[2] = R.drawable.b_star;
+//    imageSrcs[3] = R.drawable.r_star;
+//    imageSrcs[4] = R.drawable.goal;
+//
+//    // Load eyeball images
+//    eyeBallImages[0] = R.drawable.eyes_up;
+//    eyeBallImages[1] = R.drawable.eyes_down;
+//    eyeBallImages[2] = R.drawable.eyes_left;
+//    eyeBallImages[3] = R.drawable.eyes_right;
     private String[] maze;
     private int playerPos;
 
